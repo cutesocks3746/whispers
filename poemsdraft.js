e0ddcc714d71424ca5dc4f661fc8ad7735e58114
cutesocks3746 <danherbert@hey.com>
2024-11-25 22:39:41 +0530

    Update poemsdraft.js

--- poemsdraft.js
+++ poemsdraft.js
@@ -118,7 +118,7 @@ function displayPoems(poems) {
       ? poem.querySelector('[data-type="person"]').textContent === value
       : poem.querySelector('[data-type="theme"]').textContent === value;
   });
-*/
+
   // Hide/show poems
   allPoems.forEach(p => p.classList.add('hidden'));
   filteredPoems.forEach(p => p.classList.remove('hidden'));
@@ -131,7 +131,7 @@ function displayPoems(poems) {
   backButton.addEventListener('click', resetPoemView);
   mainSection.insertBefore(backButton, mainSection.firstChild);
 }
-
+*/
 function resetPoemView() {
   const allPoems = document.querySelectorAll('.poem-card');
   allPoems.forEach(p => p.classList.remove('hidden'));
